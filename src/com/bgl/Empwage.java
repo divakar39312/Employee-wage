c12f63ed74f2a61328b5d05d121de2ff3147f8d0
Divakar <divakar39312@gmail.com>
2022-07-28 15:47:22 +0530

    [divakar]:parttime files added

--- src/com/bgl/Empwage.java
+++ src/com/bgl/Empwage.java
@@ -7,13 +7,19 @@ public class Empwage {
 		System.out.println("Welcome to Employee Wage Program");
 		System.out.println("********************************\n");
 		int employeePresent = 1;
+		int employeePartTime = 2;
 		int wagePerHour = 20;
 		int numberOfHours = 8;
-		int isPresent = (int) Math.floor(Math.random() * 10) % 2;
+		int partTimeHours = 4;
+		int isPresent = (int) Math.floor(Math.random() * 10) % 3;
 		if (isPresent == employeePresent) {
 			System.out.println("Employee is Present : P");
 			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
 		} 
+		else if (isPresent == employeePartTime) {
+			System.out.println("Employee is Part Time Present : (1/2) P");
+			System.out.println("Employee earn with PartTime = " +partTimeHours * wagePerHour);
+			}
 		else {
 			System.out.println("Employee is Absent  : A");
 			System.out.println("Employee earn = 0");
