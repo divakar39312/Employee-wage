8f32aa0ee485ad863f0a17dfd6add469b8d1796f
Divakar <divakar39312@gmail.com>
2022-07-28 15:34:30 +0530

    [divakar]:files changed

---
+++ src/com/bgl/Empwage.java
@@ -0,0 +1,18 @@
+package com.bgl;
+
+public class Empwage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Program");
+		System.out.println("********************************\n");
+		int employeePresent = 1;
+		int isPresent =(int) Math.floor(Math.random()*10) % 2;
+		if ( isPresent == employeePresent)
+			System.out.println("Employee is Present : P");
+		else
+			System.out.println("Employee is Absent  : A");
+
+	}
+
+}
