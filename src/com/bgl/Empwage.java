2ef269d63fcd746416a60b166e9bc5da5860b62f
Divakar <divakar39312@gmail.com>
2022-07-28 15:52:43 +0530

    [divakar]:switchcases files added

--- src/com/bgl/Empwage.java
+++ src/com/bgl/Empwage.java
@@ -6,23 +6,24 @@ public class Empwage {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Program");
 		System.out.println("********************************\n");
-		int employeePresent = 1;
-		int employeePartTime = 2;
+		
 		int wagePerHour = 20;
 		int numberOfHours = 8;
 		int partTimeHours = 4;
 		int isPresent = (int) Math.floor(Math.random() * 10) % 3;
-		if (isPresent == employeePresent) {
+		switch (isPresent) {
+		case 1 :
 			System.out.println("Employee is Present : P");
 			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-		} 
-		else if (isPresent == employeePartTime) {
+			break;
+		case 2 :
 			System.out.println("Employee is Part Time Present : (1/2) P");
 			System.out.println("Employee earn with PartTime = " +partTimeHours * wagePerHour);
-			}
-		else {
+			break;
+		case 0 :
 			System.out.println("Employee is Absent  : A");
 			System.out.println("Employee earn = 0");
+			break;
 		}
 	}
 }
